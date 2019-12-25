d67840952608d3fea6bc512d7d34bb33649ce5d4
gosched <45745902+gosched@users.noreply.github.com>
2019-12-25 09:13:35 +0800

    Create singleton.go

---
+++ implements/go/singleton/singleton.go
@@ -0,0 +1,23 @@
+package singleton
+
+import "sync"
+
+type Singleton struct {
+	Name string
+}
+
+var singleInstance *Singleton
+var once sync.Once
+
+// GetSingleInstance .
+func GetSingleInstance() *Singleton {
+	once.Do(func() {
+		singleInstance = &Singleton{}
+	})
+	return singleInstance
+}
+
+// SetName .
+func (s *Singleton) SetName(name string) {
+	s.Name = name
+}
